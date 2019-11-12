4d4da84275ca16bb12cec6ecd34f3faad569b5f8
jacksonmwirigi@gmail.com <jacksonmwirigi@gmail.com>
2019-11-12 18:48:35 +0300

    OOP concept

--- untitled.py
+++ untitled.py
@@ -3,7 +3,6 @@ class Cab:
 cabService, location, number_plate, make  #data
 book() arrival() start()                  #methods
 
-
 class CabDriver:
    name, employee_ID, gender, national_ID #data
    openDoor() drive()                     #methods
